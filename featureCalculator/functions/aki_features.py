1a4375c45389a9e45525d3c957cbe2c3701d2358
Akihiro Maeda <amaeda10@gmail.com>
2011-12-04 21:38:20 -0800

    only capsule, commented out the capture-escape enemy

--- featureCalculator/functions/aki_features.py
+++ featureCalculator/functions/aki_features.py
@@ -14,32 +14,106 @@ To get started writing a new feature function, copy this file, rename it, and ma
 
 from game import Directions, Actions
 
-
+# state is same as successor, no action involved
 def calculate_feature_value(state, agent):  #Do not change this line    
-    return {}
-    #feature_values = {}
+    #return {}
     
-    """
-    ##Capsule feature start
-    amIRed = successor.isOnRedTeam(self.index)
+    feature_values = {}
+    
+    myPos = agent.getPosition(state) 
+    ## was using this before.. #state.getAgentState(agent.index).getPosition()
+    
+    amIRed = state.isOnRedTeam(agent.index)
     if amIRed:
-        capsulePos = successor.getBlueCapsules()
+      enemies = state.getBlueTeamIndices()
     else:
-        capsulePos = successor.getRedCapsules()
+      enemies = state.getRedTeamIndices()
+    
+    ##Get Enemy-side Capsule feature start
+    if amIRed:
+        capsulePos = state.getBlueCapsules()
+    else:
+        capsulePos = state.getRedCapsules()
     if capsulePos:
-        selfPos = self.getPosition(successor)
         #print capsulePos[1]
-        distanceToCapsule = self.getMazeDistance(capsulePos[0], selfPos)
+        distanceToCapsule = agent.getMazeDistance(capsulePos[0], myPos)
         #distanceToCapsule = abs(capsulePos[0][0]-selfPos[0])+abs(capsulePos[0][1]-selfPos[1])    
         #print distanceToCapsule
-        features['distanceToCapsule'] = distanceToCapsule
+        feature_values['distanceToCapsule'] = distanceToCapsule
     else:
-        features['distanceToCapsule'] = 0
-    #print features['distanceToCapsule']
+        feature_values['distanceToCapsule'] = 0
     ##Capsule feature ends
+    
+    """
+    ##capture / escape feature
+    if agent.isPositionInTeamTerritory(state, myPos): 
+        #I'm defending, so minimize the dis
+        feature_values['distanceToEnemyOnEnemySide'] = 0 
+        
+        for enemy in enemies:
+            # Compute distance to observed enemy
+            enemyPos = agent.getOpponentPositions(state)[enemy/2]
+            if enemyPos is None: #use estimate 
+                maxBelief = 0
+                maxPos = (1,1)
+                belief = agent.beliefs[enemy]
+                for pos in belief:
+                    if belief[pos] > maxBelief:
+                        maxBelief = belief[pos]
+                        maxPos = pos
+                enemyPos = pos
+            
+            distanceToEnemy = agent.getMazeDistance(enemyPos, myPos)
+            if distanceToEnemy == 0:
+                distanceToEnemy = 0.1
+            feature_values['distanceToEnemyOnMySide'] = 1/distanceToEnemy
+    else: 
+        #I'm offencing, so minimize the dis
+        feature_values['distanceToEnemyOnMySide'] = 0
+        
+        for enemy in enemies:
+            # Compute distance to observed enemy
+            enemyPos = agent.getOpponentPositions(state)[enemy/2]
+            if enemyPos is None: #use estimate 
+                maxBelief = 0
+                maxPos = (1,1)
+                belief = agent.beliefs[enemy]
+                for pos in belief:
+                    if belief[pos] > maxBelief:
+                        maxBelief = belief[pos]
+                        maxPos = pos
+                enemyPos = pos
+            
+            distanceToEnemy = agent.getMazeDistance(enemyPos, myPos)
+            if distanceToEnemy == 0:
+                distanceToEnemy = 0.1
+            feature_values['distanceToEnemyOnEnemySide'] = 1/distanceToEnemy
+            
+    print feature_values['distanceToEnemyOnMySide']         
+    """        
+            
+    """            
+                            #calculate maze distance to observed enemy.
+        #Note: observedEnemyPos is the position of PREVIOUS time period. 
+        #Assuming enemy is coming closer to me, I subtract 1 from mazeDis
+        mazeDisToEnemy = self.getMazeDistance(myPos, observedEnemyPos) - 1
+      
+        if mazeDisToEnemy == 0:
+          features['mazeDisToEnemy'] += -100
+        elif mazeDisToEnemy == 1:
+          features['mazeDisToEnemy'] += -5
+        elif mazeDisToEnemy == 2:
+          features['mazeDisToEnemy'] += -2  
+        else:
+          features['mazeDisToEnemy'] += -1
+        
+        ## Num of legal actions feature. If enemy is near, it's quite important 
+        # so that agent doesn't go into cal-de-sac
+        features['numLegalActions'] = len(successor.getLegalActions(self.index)) * 3
+    
     """
         
-    #return feature_values
+    return feature_values
         
         
     
